b7ed01143b6a398d32ae25f70192e3af3e049263
Mark Sundt <marksundt@gmail.com>
2017-10-15 14:35:43 -0500

    added laser on off

--- smooth.py
+++ smooth.py
@@ -2,12 +2,16 @@
 
 import math
 import time
-
+import RPI.GPIO as GPIO
 import pantilthat
 
-pantilthat.light_mode(pantilthat.PWM)
-#pantilthat.light_type(pantilthat.)
-pantilthat.brightness(255)
+GPIO.setmode(GPIO.BCM)  # set board mode to Broadcom
+
+GPIO.setup(17, GPIO.OUT)  # set up pin 17
+GPIO.setup(18, GPIO.OUT)  # set up pin 18
+
+GPIO.output(17, 1)  # turn on pin 17
+GPIO.output(18, 1)  # turn on pin 18
 
 while True:
     # Get the time in seconds
@@ -26,4 +30,4 @@ while True:
     print(round(a,2))
 
     # Sleep for a bit so we're not hammering the HAT with updates
-    time.sleep(0.005)
+    time.sleep(0.5)
